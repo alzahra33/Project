acb80c7dbecebf9f20d5defa8db495bfb6b34d33
alzahra33 <26j2180@utas.edu.om>
2025-05-20 21:19:41 +0400

    Location

--- client/src/Components/Location.js
+++ client/src/Components/Location.js
@@ -1,33 +1,29 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { getGeoLocationData } from "../Features/UserSlice";
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
 
+function Location() {
+    const [ip, setIp] = useState(null)
+    const [country, setCountry] = useState(null)
+    const [region,setRegion] = useState(null)
 
-const Location = () =>{
-  const [ip , setIp] = useState(null); //State to hold the IP address
-  const [country , setCountry] = useState(null); //State to hold geolocation
-  const [region , setRegion] = useState(null); // State to hold geolocation
-  useEffect(() => {
-    getGeoLocationData();
-}, []);
-const getGeoLocationData = async () => {
-  try { 
-    const response = await axios.get("https://geo.ipify.org/api/v2/country?apiKey=at_10tfuYpbJdQW3liL6iF95tTik8Gtv");
-    setIp(response.data.ip);
-    setCountry(response.data.location.country); // Set country
-    setRegion(response.data.location.region); // Set region
-   
-  } catch (error) {
-    console.error("Error fetching geolocation data:", error.message);
-  }
-};
-return (
-  <div>
-    <h6>{ip}</h6>
-    <h6>{country}</h6>
-    <h6>{region}</h6>
-  </div>
-);
+    const getGeoLocation = async()=>{
+        const response = await axios.get(`${process.env.REACT_APP_LOCAION_API_KEY}`)
+
+        setIp(response.data.ip)
+        setCountry(response.data.location.country)
+        setRegion(response.data.location.region)
+    }
+    useEffect(()=>{
+      getGeoLocation()
+    },[])
+  return (
+    <div>
+      <h6>Ip Address: {ip}</h6>  
+      <h6>Country: {country}</h6>  
+      <h6>Region: {region}</h6>  
+    </div>
+  )
 }
-export default Location;
+
+export default Location
